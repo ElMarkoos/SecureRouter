36e2ef04f8186391ab0cf0cacf082c90bbd39e05
ElMarkoos <112957789+ElMarkoos@users.noreply.github.com>
2025-05-10 18:00:27 +0200

    ME QUIERO PEGAR TRES TIROS Y MEDIO

--- app/src/main/java/com/tfg/securerouter/ui/theme/Color.kt
+++ app/src/main/java/com/tfg/securerouter/ui/theme/Color.kt
@@ -1,27 +1,93 @@
-/*
- * Copyright (C) 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.tfg.securerouter.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// EXTRAS
+// STATUS
+val ConnectedStatusLight = Color(0xFF00FF00)
+val OnConnectedStatusLight = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DisconnectedStatusLight = Color(0xFF9B9B9B)
+val OnDisconnectedStatusLight = Color(0xFFFFFFFF)
+
+val ErrorStatusLight = Color(0xFFFF0000)
+val OnErrorStatusLight = Color(0xFFFFFFFF)
+
+// BACKGROUND
+val BackgroundLight = Color(0XFFFFFFFF)
+val OnBackgroundLight = Color(0xFF000000)
+
+// PRIMARY
+val PrimaryLight = Color(0xFF0000FF)
+val OnPrimaryLight = Color(0XFFFFFFFF)
+
+val PrimaryContainerLight = Color(0xFF0000FF)
+val OnPrimaryContainerLight = Color(0XFFFFFFFF)
+
+//val PrimaryFixedLight = Color(0XFFFFFFFF)
+//val OnPrimaryFixedLight = Color(0XFFFFFFFF)
+//
+//val PrimaryFixedDimLight = Color(0XFFFFFFFF)
+//val OnPrimaryFixedVariantLight = Color(0XFFFFFFFF)
+
+val InversePrimaryLight = Color(0XFFFFFFFF)
+
+// SECONDARY
+val SecondaryLight = Color(0XFFFFFFFF)
+val OnSecondaryLight = Color(0XFFFFFFFF)
+
+val SecondaryContainerLight = Color(0XFFFFFFFF)
+val OnSecondaryContainerLight = Color(0XFFFFFFFF)
+
+//val SecondaryFixedLight = Color(0XFFFFFFFF)
+//val OnSecondaryFixedLight = Color(0XFFFFFFFF)
+//
+//val SecondaryFixedDimLight = Color(0XFFFFFFFF)
+//val OnSecondaryFixedVariantLight = Color(0XFFFFFFFF)
+
+// TERTIARY
+val TertiaryLight = Color(0XFFFFFFFF)
+val OnTertiaryLight = Color(0XFFFFFFFF)
+
+val TertiaryContainerLight = Color(0XFFFFFFFF)
+val OnTertiaryContainerLight = Color(0XFFFFFFFF)
+
+//val TertiaryFixedLight = Color(0XFFFFFFFF)
+//val OnTertiaryFixedLight = Color(0XFFFFFFFF)
+//
+//val TertiaryFixedDimLight = Color(0XFFFFFFFF)
+//val OnTertiaryFixedVariantLight = Color(0XFFFFFFFF)
+
+// SURFACE
+val SurfaceLight = Color(0xFFA4A4A4)
+val OnSurfaceLight = Color(0xFF000000)
+
+val SurfaceBrightLight = Color(0XFFFFFFFF)
+
+val SurfaceContainerLight = Color(0XFFFFFFFF)
+val SurfaceContainerHighLight = Color(0XFFFFFFFF)
+val SurfaceContainerHighestLight = Color(0XFFFFFFFF)
+val SurfaceContainerLowLight = Color(0XFFFFFFFF)
+val SurfaceContainerLowestLight = Color(0XFFFFFFFF)
+
+//val SurfaceDimLight = Color(0XFFFFFFFF)
+//val SurfaceTintLight = Color(0XFFFFFFFF)
+
+val SurfaceVariantLight = Color(0XFFFFFFFF)
+val OnSurfaceVariantLight = Color(0XFFFFFFFF)
+
+val InverseSurfaceLight = Color(0XFFFFFFFF)
+val InverseOnSurfaceLight = Color(0XFFFFFFFF)
+
+// ERROR
+val ErrorLight = Color(0XFFFFFFFF)
+val OnErrorLight = Color(0XFFFFFFFF)
+val ErrorContainerLight = Color(0XFFFFFFFF)
+val OnErrorContainerLight = Color(0XFFFFFFFF)
+
+// LINE
+val OutlineLight = Color(0XFFFFFFFF)
+val OutlineVariantLight = Color(0XFFFFFFFF)
+
+// SCRIM
+val ScrimLight = Color(0XFFFFFFFF)
--- app/src/main/java/com/tfg/securerouter/ui/theme/Theme.kt
+++ app/src/main/java/com/tfg/securerouter/ui/theme/Theme.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.tfg.securerouter.ui.theme
 
 import android.app.Activity
@@ -25,60 +9,210 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+data class ExtraColors(
+    val connectedStatus: Color = Color.White,
+    val onConnectedStatus: Color = Color.White,
+    val disconnectedStatus: Color = Color.White,
+    val onDisconnectedStatus: Color = Color.White,
+    val errorStatus: Color = Color.White,
+    val onErrorStatus: Color = Color.White,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+val LocalExtraColors = compositionLocalOf<ExtraColors> {
+    error("No ExtraColors provided")
+}
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val ExtraColorsLight = ExtraColors(
+    connectedStatus = ConnectedStatusLight,
+    onConnectedStatus = OnConnectedStatusLight,
+    disconnectedStatus = DisconnectedStatusLight,
+    onDisconnectedStatus = OnDisconnectedStatusLight,
+    errorStatus = ErrorStatusLight,
+    onErrorStatus = OnErrorStatusLight
+)
+
+private val ExtraColorsDark = ExtraColors(
+    connectedStatus = ConnectedStatusLight,
+    onConnectedStatus = OnConnectedStatusLight,
+    disconnectedStatus = DisconnectedStatusLight,
+    onDisconnectedStatus = OnDisconnectedStatusLight,
+    errorStatus = ErrorStatusLight,
+    onErrorStatus = OnErrorStatusLight
+)
+
+
+private val LightColorScheme = darkColorScheme(
+    background = BackgroundLight,
+    onBackground = OnBackgroundLight,
+
+    primary = PrimaryLight,
+    onPrimary = OnPrimaryLight,
+
+    primaryContainer = PrimaryContainerLight,
+    onPrimaryContainer = OnPrimaryContainerLight,
+
+    // primaryFixed = PrimaryFixedLight,
+    //onPrimaryFixed = OnPrimaryFixedLight,
+    //
+    //primaryFixedDim = PrimaryFixedDimLight,
+    //onPrimaryFixedVariant = OnPrimaryFixedVariantLight,
+
+    inversePrimary = InversePrimaryLight,
+
+    secondary = SecondaryLight,
+    onSecondary = OnSecondaryLight,
+
+    secondaryContainer = SecondaryContainerLight,
+    onSecondaryContainer = OnSecondaryContainerLight,
+
+    //secondaryFixed = SecondaryFixedLight,
+    //onSecondaryFixed = OnSecondaryFixedLight,
+    //
+    //secondaryFixedDim = SecondaryFixedDimLight,
+    //onSecondaryFixedVariant = OnSecondaryFixedVariantLight,
+
+    tertiary = TertiaryLight,
+    onTertiary = OnTertiaryLight,
+
+    tertiaryContainer = TertiaryContainerLight,
+    onTertiaryContainer = OnTertiaryContainerLight,
+
+    //tertiaryFixed = TertiaryFixedLight,
+    //onTertiaryFixed = OnTertiaryFixedLight,
+    //
+    //tertiaryFixedDim = TertiaryFixedDimLight,
+    //onTertiaryFixedVariant = OnTertiaryFixedVariantLight,
+
+    surface = SurfaceLight,
+    onSurface = OnSurfaceLight,
+
+    surfaceBright = SurfaceBrightLight,
+
+    surfaceContainer = SurfaceContainerLight,
+    surfaceContainerHigh = SurfaceContainerHighLight,
+    surfaceContainerHighest = SurfaceContainerHighestLight,
+    surfaceContainerLow = SurfaceContainerLowLight,
+    surfaceContainerLowest = SurfaceContainerLowestLight,
+
+    //surfaceDim = SurfaceDimLight,
+    //surfaceTint = SurfaceTintLight,
+
+    surfaceVariant = SurfaceVariantLight,
+    onSurfaceVariant = OnSurfaceVariantLight,
+
+    inverseSurface = InverseSurfaceLight,
+    inverseOnSurface = InverseOnSurfaceLight,
+
+    error = ErrorLight,
+    onError = OnErrorLight,
+    errorContainer = ErrorContainerLight,
+    onErrorContainer = OnErrorContainerLight,
+
+    outline = OutlineLight,
+    outlineVariant = OutlineVariantLight,
+
+    scrim = ScrimLight
+)
+
+private val DarkColorScheme = lightColorScheme(
+    background = BackgroundLight,
+    onBackground = OnBackgroundLight,
+
+    primary = PrimaryLight,
+    onPrimary = OnPrimaryLight,
+
+    primaryContainer = PrimaryContainerLight,
+    onPrimaryContainer = OnPrimaryContainerLight,
+
+    // primaryFixed = PrimaryFixedLight,
+    //onPrimaryFixed = OnPrimaryFixedLight,
+    //
+    //primaryFixedDim = PrimaryFixedDimLight,
+    //onPrimaryFixedVariant = OnPrimaryFixedVariantLight,
+
+    inversePrimary = InversePrimaryLight,
+
+    secondary = SecondaryLight,
+    onSecondary = OnSecondaryLight,
+
+    secondaryContainer = SecondaryContainerLight,
+    onSecondaryContainer = OnSecondaryContainerLight,
+
+    //secondaryFixed = SecondaryFixedLight,
+    //onSecondaryFixed = OnSecondaryFixedLight,
+    //
+    //secondaryFixedDim = SecondaryFixedDimLight,
+    //onSecondaryFixedVariant = OnSecondaryFixedVariantLight,
+
+    tertiary = TertiaryLight,
+    onTertiary = OnTertiaryLight,
+
+    tertiaryContainer = TertiaryContainerLight,
+    onTertiaryContainer = OnTertiaryContainerLight,
+
+    //tertiaryFixed = TertiaryFixedLight,
+    //onTertiaryFixed = OnTertiaryFixedLight,
+    //
+    //tertiaryFixedDim = TertiaryFixedDimLight,
+    //onTertiaryFixedVariant = OnTertiaryFixedVariantLight,
+
+    surface = SurfaceLight,
+    onSurface = OnSurfaceLight,
+
+    surfaceBright = SurfaceBrightLight,
+
+    surfaceContainer = SurfaceContainerLight,
+    surfaceContainerHigh = SurfaceContainerHighLight,
+    surfaceContainerHighest = SurfaceContainerHighestLight,
+    surfaceContainerLow = SurfaceContainerLowLight,
+    surfaceContainerLowest = SurfaceContainerLowestLight,
+
+    //surfaceDim = SurfaceDimLight,
+    //surfaceTint = SurfaceTintLight,
+
+    surfaceVariant = SurfaceVariantLight,
+    onSurfaceVariant = OnSurfaceVariantLight,
+
+    inverseSurface = InverseSurfaceLight,
+    inverseOnSurface = InverseOnSurfaceLight,
+
+    error = ErrorLight,
+    onError = OnErrorLight,
+    errorContainer = ErrorContainerLight,
+    onErrorContainer = OnErrorContainerLight,
+
+    outline = OutlineLight,
+    outlineVariant = OutlineVariantLight,
+
+    scrim = ScrimLight
 )
 
 @Composable
-fun MyApplicationTheme(
+fun SecureRouterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    val extraColors = if (darkTheme) ExtraColorsDark else ExtraColorsLight
+
+    CompositionLocalProvider(LocalExtraColors provides extraColors) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
